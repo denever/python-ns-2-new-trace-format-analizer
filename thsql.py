be68d883767c154f8ab2fc3ca687d824b660c9e1
Giuseppe "denever" Martino <martinogiuseppe@gmail.com>
2008-09-09 09:16:01 +0200

    Added Average Delay Cal

--- thsql.py
+++ thsql.py
@@ -3,13 +3,14 @@ from NS2NewTraceSql import NS2NewTraceSql
 
 mio = NS2NewTraceSql('prova.pta')
 
-sent_pkts = mio.get_sent_pkts_times_at(1,1,'MAC')
-recv_pkts = mio.get_recv_pkts_times_at(0,1,'MAC')
+sent_pkts = mio.get_sent_pkts_times_at(1,1)
+recv_pkts = mio.get_recv_pkts_times_at(0,1)
 
 start_time = Decimal(sent_pkts[0][1])
 last_sent_pkt_id = sent_pkts[-1][0]
 
-print len(sent_pkts), len(recv_pkts)
+print 'Sent pkts at node',len(sent_pkts)
+print 'Recv pkts at node',len(recv_pkts)
 
 for recv_pkt in recv_pkts:
     if recv_pkt[0] == last_sent_pkt_id:
@@ -18,11 +19,7 @@ for recv_pkt in recv_pkts:
 stop_time = Decimal(stop_time)
 delta = stop_time - start_time
 
-print start_time, stop_time, delta
-
 data_size = Decimal(mio.get_recv_flow_total_size_at(0,1,20))
-
-print data_size
 
 th_bps = data_size / delta
 
@@ -30,9 +27,13 @@ conv = Decimal(8) / Decimal(1000)
 
 th_kbps = th_bps*conv
 
-print th_kbps
+print 'Throughtput',th_kbps
 
-pippo = (data_size / Decimal(9)) * conv
-
-print pippo
-
+delay = Decimal('0')
+mio = int(0)
+for s,r in zip(sent_pkts,recv_pkts):
+    delay += Decimal(r[1]) - Decimal(s[1])
+    mio += 1
+    
+avgDelay = delay / Decimal(mio)
+print 'avgDelay', avgDelay * Decimal(1000)
